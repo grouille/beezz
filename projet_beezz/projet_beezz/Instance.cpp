c05da81536b565baeb118bc04356179f2fb79b3a
pcpret <pcpret@LNE-5CG5124YR5.ad.inria.fr>
2020-03-06 18:22:06 +0100

    ALgorithm progressing

--- projet_beezz/projet_beezz/Instance.cpp
+++ projet_beezz/projet_beezz/Instance.cpp
@@ -12,3 +12,71 @@ Instance::~Instance()
 void Instance::lire_instance_a_partir_dun_fichier_csv()
 {
 }
+
+int Instance::get_nb_options_of_parametre(int i)
+{
+	if (i < 0 || i >= nb_parametres)
+		throw invalid_argument("Attention!!!");
+	return nb_options_par_parametre[i];
+}
+
+double Instance::get_note_parametre_option_critere(int i, int j, int c)
+{
+	if (i < 0 || i >= nb_parametres ||
+		j < 0 || j >= get_nb_options_of_parametre(i) ||
+		c < 0 || c >= nb_criteres)
+		throw invalid_argument("Attention!!!");
+	return valeur_parametre_option_critere[i][j][c];
+}
+
+double Instance::get_prix_parametre_option(int i, int j)
+{
+	if (i < 0 || i >= nb_parametres ||
+		j < 0 || j >= get_nb_options_of_parametre(i))
+		throw invalid_argument("Attention!!!");
+	return valeur_parametre_option_critere[i][j][6];
+}
+
+double Instance::get_facteur_parametre_option(int i, int j)
+{
+	if (i < 0 || i >= nb_parametres ||
+		j < 0 || j >= get_nb_options_of_parametre(i))
+		throw invalid_argument("Attention!!!");
+	return valeur_parametre_option_critere[i][j][7];
+}
+
+double Instance::get_duree_de_vie_parametre_option(int i, int j)
+{
+	if (i < 0 || i >= nb_parametres ||
+		j < 0 || j >= get_nb_options_of_parametre(i))
+		throw invalid_argument("Attention!!!");
+	return valeur_parametre_option_critere[i][j][8];
+}
+
+int Instance::get_souhait_client_pour_critere(int c)
+{
+
+	if (c < 0 || c >= nb_criteres)
+		throw invalid_argument("Attention!!!");
+	return souhait_critere[c];
+}
+
+int Instance::get_nb_parametres()
+{
+	return nb_parametres;
+}
+
+int Instance::get_nb_criteres()
+{
+	return nb_criteres;
+}
+
+int Instance::get_nb_prix_max()
+{
+	return prix_max;
+}
+
+int Instance::get_duree_de_vie_minimale()
+{
+	return duree_de_vie_minimale;
+}
--- projet_beezz/projet_beezz/Instance.h
+++ projet_beezz/projet_beezz/Instance.h
@@ -10,8 +10,14 @@ using namespace std;
 class Instance
 {
 public:
-	int nb_parameter;
-	vector<int> nb_option_par_parameter;
+	int nb_parametres;
+	int nb_criteres;
+	vector<int> nb_options_par_parametre;
+	vector<vector<vector<double>>> valeur_parametre_option_critere;
+	vector<int> souhait_critere;
+	int prix_max;
+	int duree_de_vie_minimale;
+
 public:
 	Instance(string file_name);
 	~Instance();
@@ -21,9 +27,18 @@ public:
 
 
 	//getter methods
-	int get_nb_options_of_parametr(int i);
-	int get_note_parameter_option(int i, int j);
+	int get_nb_options_of_parametre(int i);
+	double get_note_parametre_option_critere(int i, int j, int c);
+	double get_prix_parametre_option(int i, int j);
+	double get_facteur_parametre_option(int i, int j);
+	double get_duree_de_vie_parametre_option(int i, int j);
 
+	int get_souhait_client_pour_critere(int c);
+
+	int get_nb_parametres();
+	int get_nb_criteres();
+	int get_nb_prix_max();
+	int get_duree_de_vie_minimale();
 
 };
 
