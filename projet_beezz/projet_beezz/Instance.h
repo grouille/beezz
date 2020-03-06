380967bd20b831027fdeb00630e4de5288adc7c1
Kaba Keita <kaba.keita@inria.fr>
2020-03-06 15:44:48 +0100

    changement dans instance

--- projet_beezz/projet_beezz/Instance.h
+++ projet_beezz/projet_beezz/Instance.h
@@ -3,16 +3,26 @@
 
 #pragma once
 #include <iostream>
+#include<vector>
 
 using namespace std;
 
 class Instance
 {
 public:
+	int nb_parameter;
+	vector<int> nb_option_par_parameter;
+public:
 	Instance(string file_name);
 	~Instance();
 
+	//setter methods
 	void lire_instance_a_partir_dun_fichier_csv();
+
+
+	//getter methods
+	int get_nb_options_of_parametr(int i);
+	int get_note_parameter_option(int i, int j);
 
 
 };
