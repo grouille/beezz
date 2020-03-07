79af125c5beeecf840e2a46394cf88501c28db25
pcpret <pcpret@LNE-5CG5124YR5.ad.inria.fr>
2020-03-07 11:24:56 +0100

    add solutions.txt pour representer les individus

--- projet_beezz/projet_beezz/main.cpp
+++ projet_beezz/projet_beezz/main.cpp
@@ -1,20 +1,18 @@
 #include "AlgoGenetique.h"
-
+#include <chrono>
 int main()
 {
 	Instance instance("../../parametres_INRIA.csv", "../../data.txt");
 
 	int nb_iterations = 500;
 	double temps_max = 100;
-<<<<<<< HEAD
-	int nb_individus = 30;
-
-=======
 	int nb_individus = 150;
->>>>>>> branch 'master' of https://github.com/grouille/beezz.git
 	AlgoGenetique algo_genetique(&instance, nb_iterations, temps_max, nb_individus);
+	chrono::milliseconds _chronoStart = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
 	algo_genetique.run();
-
+	chrono::milliseconds _chronoEnd = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
+	chrono::milliseconds _chrono = _chronoEnd - _chronoStart;
+	cout << "L'algorithme a pris\t | " << float(_chrono.count()) / 1000 << " sec |" << endl << endl;
 	string file_name = "../../solutions.txt";
 	ofstream solutions;
 	for (int i = 0; i < algo_genetique.best_individus.size(); i++)
